264a08351aa624337508071d40efdcb02c9d16cd
Josh Collins <jjcollins2@crimson.ua,edu>
2017-11-26 18:09:33 -0600

    Added PHP file to test basic connection success

---
+++ initialConnect.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshcollins
+ * Date: 11/26/17
+ * Time: 5:41 PM
+ */
+
+$servername = "PRIVATE";
+$username = "PRIVATE";
+$password = "PRIVATE";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+
